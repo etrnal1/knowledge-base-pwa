23b08bd2d837f36fb288f94ef503936f7015a6ef
etrnal1 <1026177564@qq.com>
2026-03-16 10:06:00 +0800

    添加: SW自动更新 + 密码保护 + 分享 + 笔记过期时间

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_VERSION = 'kb-pwa-v5'
 const STATIC_CACHE = `knowledge-base-static-${CACHE_VERSION}`
 
 // 构建时由 vite 插件自动注入资源列表，不再运行时 fetch manifest
-const BUILD_ASSETS = ["./assets/index-jrvo1zkW.js","./assets/index-DXsXlBKx.css"]
+const BUILD_ASSETS = ["./assets/index-CshwP9_x.js","./assets/index-DbV4Mn7G.css"]
 
 const APP_SHELL = [
   './',
