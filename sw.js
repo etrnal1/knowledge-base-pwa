8c1f107ae63ca7f7053a976e535a759cd04c5812
etrnal1 <1026177564@qq.com>
2026-03-16 09:51:02 +0800

    修复: 笔记功能数据库查询兼容性问题

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_VERSION = 'kb-pwa-v5'
 const STATIC_CACHE = `knowledge-base-static-${CACHE_VERSION}`
 
 // 构建时由 vite 插件自动注入资源列表，不再运行时 fetch manifest
-const BUILD_ASSETS = ["./assets/index-WGhDfaza.js","./assets/index-C4hY8yVZ.css"]
+const BUILD_ASSETS = ["./assets/index-CIF1wXDG.js","./assets/index-C4hY8yVZ.css"]
 
 const APP_SHELL = [
   './',
